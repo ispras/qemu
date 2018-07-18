dc7d35df5c3ff8000b8d9f1eb61ba025536ffbca
Vasiliev Ivan <vasiliev@ispras.ru>
2018-07-18 14:16:22 +0300

    file_monitor: couple of fixes

--- plugins/plugins_src/file_monitor.c
+++ plugins/plugins_src/file_monitor.c
@@ -36,104 +36,120 @@ static void cpus_exit(const PluginInterface *pi)
 static void syscall_open_cb(void *msg, CPUArchState *env)
 {
     Parameters_oc *params = (Parameters_oc *) msg;
-    if (!params->ret)
-    {
-        fprintf(syscallfile, "OPEN\n");
-        fprintf(syscallfile, "\tName: %s\n", params->name);
-        fprintf(syscallfile, "\tHandle: %"PRIx64"\n", params->handle);
-        fprintf(syscallfile, "\tAccess: %x\n", params->access);
+    if (!params->ret) {
         Parameters_oc *params_cpy = g_malloc(sizeof(Parameters_oc));
         memcpy(params_cpy, params, sizeof(Parameters_oc));
         params_cpy->name = g_strdup(params->name);
         hm_insert(hm, params->handle, get_current_context(), params_cpy);
     }
-    else
-    {
-        fprintf(syscallfile, "File %s cannot open\n", params->name);
+    if (syscallfile) {
+        if (!params->ret)
+        {
+            fprintf(syscallfile, "OPEN\n");
+            fprintf(syscallfile, "\tName: %s\n", params->name);
+            fprintf(syscallfile, "\tHandle: %"PRIx64"\n", params->handle);
+            fprintf(syscallfile, "\tAccess: %x\n", params->access);
+        }
+        else
+        {
+            fprintf(syscallfile, "File %s cannot be opened\n", params->name);
+        }
     }
 }
 
 static void syscall_create_cb(void *msg, CPUArchState *env)
 {
     Parameters_oc *params = (Parameters_oc *) msg;
-    if (!params->ret)
-    {
-        fprintf(syscallfile, "CREATE\n");
-        fprintf(syscallfile, "\tName: %s\n", params->name);
-        fprintf(syscallfile, "\tHandle: %"PRIx64"\n", params->handle);
-        fprintf(syscallfile, "\tAccess: %x\n", params->access);
+    if (!params->ret) {
         Parameters_oc *params_cpy = g_malloc(sizeof(Parameters_oc));
         memcpy(params_cpy, params, sizeof(Parameters_oc));
         params_cpy->name = g_strdup(params->name);
         hm_insert(hm, params->handle, get_current_context(), params_cpy);
     }
-    else
-    {
-        fprintf(syscallfile, "File %s cannot open\n", params->name);
+    if (syscallfile) {
+        if (!params->ret)
+        {
+            fprintf(syscallfile, "CREATE\n");
+            fprintf(syscallfile, "\tName: %s\n", params->name);
+            fprintf(syscallfile, "\tHandle: %"PRIx64"\n", params->handle);
+            fprintf(syscallfile, "\tAccess: %x\n", params->access);
+        }
+        else
+        {
+            fprintf(syscallfile, "File %s cannot be opened\n", params->name);
+        }
     }
 }
 
 static void syscall_read_cb(void *msg, CPUArchState *env)
 {
     Parameters_rw *params = (Parameters_rw *) msg;
-    if (!params->ret)
-    {
-        fprintf(syscallfile, "READ\n");
-        fprintf(syscallfile, "\tHandle: %"PRIx64"\n", params->handle);
-        fprintf(syscallfile, "\tLength: %x\n", params->length);
-        fprintf(syscallfile, "\tBuffer: ");
-        int i = 0;
-        for (; i < params->length; i++)
+    if (syscallfile) {
+        if (!params->ret)
         {
-            fprintf(syscallfile, "%02x ", params->buffer[i]);
+            fprintf(syscallfile, "READ\n");
+            fprintf(syscallfile, "\tHandle: %"PRIx64"\n", params->handle);
+            fprintf(syscallfile, "\tLength: %x\n", params->length);
+            fprintf(syscallfile, "\tBuffer: ");
+            int i = 0;
+            for (; i < params->length; i++)
+            {
+                fprintf(syscallfile, "%02x ", params->buffer[i]);
+            }
+            fprintf(syscallfile, "\n");
         }
-        fprintf(syscallfile, "\n");
-    }
-    else
-    {
-        fprintf(syscallfile, "File %"PRIx64" cannot read\n", params->handle);
+        else
+        {
+            fprintf(syscallfile, "File %"PRIx64" cannot read\n", params->handle);
+        }
     }
 }
 
 static void syscall_write_cb(void *msg, CPUArchState *env)
 {
     Parameters_rw *params = (Parameters_rw *) msg;
-    if (!params->ret)
-    {
-        fprintf(syscallfile, "WRITE\n");
-        fprintf(syscallfile, "\tHandle: %"PRIx64"\n", params->handle);
-        fprintf(syscallfile, "\tLength: %x\n", params->length);
-        fprintf(syscallfile, "\tBuffer: ");
-        int i = 0;
-        for (; i < params->length; i++)
+    if (syscallfile) {
+        if (!params->ret)
         {
-            fprintf(syscallfile, "%02x ", params->buffer[i]);
+            fprintf(syscallfile, "WRITE\n");
+            fprintf(syscallfile, "\tHandle: %"PRIx64"\n", params->handle);
+            fprintf(syscallfile, "\tLength: %x\n", params->length);
+            fprintf(syscallfile, "\tBuffer: ");
+            int i = 0;
+            for (; i < params->length; i++)
+            {
+                fprintf(syscallfile, "%02x ", params->buffer[i]);
+            }
+            fprintf(syscallfile, "\n");
         }
-        fprintf(syscallfile, "\n");
-    }
-    else
-    {
-        fprintf(syscallfile, "File %"PRIx64" cannot write\n", params->handle);
+        else
+        {
+            fprintf(syscallfile, "File %"PRIx64" cannot write\n", params->handle);
+        }
     }
 }
 
 static void syscall_duplicate_cb(void *msg, CPUArchState *env)
 {
     Parameters_do *params = (Parameters_do *) msg;
-    fprintf(syscallfile, "DUPLICATE\n");
-    fprintf(syscallfile, "\tSourceHandle: %x\n", (int) params->sourceHandle);
-    fprintf(syscallfile, "\tTargetHandle: %x\n", (int) params->pTargetHandle);
+    if (syscallfile) {
+        fprintf(syscallfile, "DUPLICATE\n");
+        fprintf(syscallfile, "\tSourceHandle: %x\n", (int) params->sourceHandle);
+        fprintf(syscallfile, "\tTargetHandle: %x\n", (int) params->pTargetHandle);
+    }
     
     Parameters_oc *params_open = hm_find(hm, params->sourceHandle, get_current_context());
     if (params_open)
     {
-        Parameters_oc *params_new = g_malloc(sizeof(Parameters_oc));
-        fprintf(syscallfile, "we found the handle\n");
-        if (params_open->name) {
-            fprintf(syscallfile, "\tName: %s\n", params_open->name);
+        if (syscallfile) {
+            fprintf(syscallfile, "we found the handle\n");
+            if (params_open->name) {
+                fprintf(syscallfile, "\tName: %s\n", params_open->name);
+            }
+            fprintf(syscallfile, "\tHandle: %"PRIx64"\n", params_open->handle);
+            fprintf(syscallfile, "\tAccess: %x\n", params_open->access);
         }
-        fprintf(syscallfile, "\tHandle: %"PRIx64"\n", params_open->handle);
-        fprintf(syscallfile, "\tAccess: %x\n", params_open->access);
+        Parameters_oc *params_new = g_malloc(sizeof(Parameters_oc));
         if (params_open->name) {
             params_new->name = g_strdup(params_open->name);
         }
@@ -148,11 +164,15 @@ static void syscall_close_cb(void *msg, CPUArchState *env)
     Parameters_c *params = (Parameters_c *) msg;
     if (hm_find(hm, params->handle, get_current_context()))
     {
-        fprintf(syscallfile, "CLOSE\n");
+        if (syscallfile) {
+            fprintf(syscallfile, "CLOSE\n");
+        }
         //fprintf(syscallfile, "\tHandle: %x\n", params->handle);
         Parameters_oc *params_oc = hm_find(hm, params->handle, get_current_context());
         if (params_oc) {
-            fprintf(syscallfile, "\tName: %s\n\tHandle: %"PRIx64"\n", (params_oc->name) ? params_oc->name : "", params->handle);
+            if (syscallfile) {
+                fprintf(syscallfile, "\tName: %s\n\tHandle: %"PRIx64"\n", (params_oc->name) ? params_oc->name : "", params->handle);
+            }
             if (params_oc->name) {
                 g_free(params_oc->name);
             }
@@ -167,6 +187,14 @@ static HandleMap *get_files_list(void)
     return hm;
 }
 
+static void start_syscall_log(Monitor *mon, const QDict *qdict)
+{
+    const char *fname = "syscall_general.log";
+    syscallfile = fopen(fname, "w");
+    if (!syscallfile)
+        monitor_printf(mon, "Can\'t open file %s\n", fname);
+}
+
 void pi_start(PluginInterface *pi)
 {
     pi->exit = cpus_exit;
@@ -174,6 +202,21 @@ void pi_start(PluginInterface *pi)
     static const struct fileMonFuncs funcs = { .f1 = get_files_list};
     pi->funcs = &funcs;
 
+    static mon_cmd_t mon_cmds[] = {
+        {
+            .name       = "enable_filemon_log",
+            .args_type  = "",
+            .params     = "",
+            .help       = "begin logging of syscalls",
+            .cmd = start_syscall_log,
+        },
+        {
+            .name       = NULL,
+        },
+    };
+    pi->cmd_table = mon_cmds;
+
+    hm = hm_new();
     char name[] = "syscall";
 
     plugin_subscribe(syscall_create_cb, name, "VMI_SC_CREATE");
@@ -182,11 +225,4 @@ void pi_start(PluginInterface *pi)
     plugin_subscribe(syscall_write_cb, name, "VMI_SC_WRITE");
     plugin_subscribe(syscall_duplicate_cb, name, "VMI_SC_DUPLICATE_OBJ");
     plugin_subscribe(syscall_close_cb, name, "VMI_SC_CLOSE");
-
-    const char *fname = "syscall_general.log";
-    syscallfile = fopen(fname, "w");
-    if (!syscallfile)
-        fprintf(pi->output, "Can\'t read file %s\n", fname);
-
-    hm = hm_new();
 }
